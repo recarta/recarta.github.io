0d731e7ca1f739ddd23e32379afc601b4181d470
SwissNight <etienne.friedli@gmail.com>
2024-03-22 15:55:55 +0100

    1884c26

---
+++ assets/plugin-vue_export-helper.c66e07c4.js
@@ -0,0 +1,1 @@
+import{c as s}from"./index.de9c2cc7.js";const u={dark:{type:Boolean,default:null}};function c(r,a){return s(()=>r.dark===null?a.dark.isActive:r.dark)}var k=(r,a)=>{const t=r.__vccOpts||r;for(const[e,o]of a)t[e]=o;return t};export{k as _,c as a,u};
