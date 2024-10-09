ad0fc4fa13871e958fa2b9ca439810e41a870b6a
SwissNight <etienne.friedli@gmail.com>
2024-10-09 12:08:54 +0200

    ecd948c

---
+++ assets/quasar-lang-pack.cacdc207.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.8d7ad993.js";var r=a(async()=>{});export{r as default};
---
+++ assets/use-quasar.18f2beab.js
@@ -0,0 +1,1 @@
+import{i as a,a8 as r}from"./index.8d7ad993.js";function u(){return a(r)}export{u};
