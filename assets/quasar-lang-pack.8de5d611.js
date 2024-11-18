9b54154c327ead4240e43d8333097c624cee803a
Etienne Friedli <etienne@recarta.ai>
2024-11-18 11:34:46 +0100

    e069bd6

---
+++ assets/quasar-lang-pack.8de5d611.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.ecd8a84a.js";var r=a(async()=>{});export{r as default};
---
+++ assets/use-quasar.9fba0fd6.js
@@ -0,0 +1,1 @@
+import{i as a,a8 as r}from"./index.ecd8a84a.js";function u(){return a(r)}export{u};
