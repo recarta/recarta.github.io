c8603700ac1692431295c0a5f0e2fc7c13e5b3e1
Etienne Friedli <etienne@recarta.ai>
2025-07-15 14:32:41 +0200

    18f0800

---
+++ assets/quasar-lang-pack.54c7bd6c.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.e2ac4dce.js";var r=a(async()=>{});export{r as default};
---
+++ assets/use-quasar.bbfd003b.js
@@ -0,0 +1,1 @@
+import{i as a,a8 as r}from"./index.e2ac4dce.js";function u(){return a(r)}export{u};
